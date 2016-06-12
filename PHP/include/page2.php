6d028294898c3f30b7df724ad06ef69c496c436b
Ncapron <nicolascapron403@gmail.com>
2016-06-12 23:26:32 +0200

    include

---
+++ PHP/include/page2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon super site</title>
+    </head>
+ 
+    <body>
+ 
+    <?php include("entete.php"); ?>
+    
+    <?php include("menu.php"); ?>
+    
+    <!-- Le corps -->
+    
+    <div id="corps">
+        <h1>Plus d'infos</h1>
+        
+        <p>
+            Sur cette page je vais vous parler un peu plus dans le détail de moi !<br />
+            Qui suis-je ? quelle est ma vie, mon oeuvre?
+        </p>
+    </div>
+    
+    <!-- Le pied de page -->
+    
+    <?php include("pieddepage.php")
+    
+    </body>
+</html>
---
+++ PHP/premier_test/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Ceci est une page test</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>page de test</h2>
+
+		<p>
+			Cette page contient du code html avec des balises PHP.<br />
+			<?php echo "Bonjour !"; ?>
+			Voici quelques tests
+		</p>
+		<ul>
+			<li style="color: blue;">Texte bleue</li>
+			<li style="color: red;">Texte rouge</li>
+			<li style="color: green;">Texte vert</li>
+		</ul>
+	</body>
+</html>
